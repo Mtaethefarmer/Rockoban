d292cac0f04c453b0782efd2d6fba67c01506ff8
Devone Reynolds <mtae000@gmail.com>
2019-09-25 22:20:39 -0700

    Day 3
    Goal: Have 2 players move on screen. [Complete]
    
    DAY 3[Good]
    
    I feel good about getting two players moving on screen, this demonstrates the ability to use the controller to play a co-op game.
    [Improvements]
    - Having two controllers ready would be nice. I had to quickly search for another controller that would work on my machine in the middle of the sprint which ate up some of my time. Time which I am now realizing is precious since there is so little of it.
    - I should probably have each player be instanced when a controller is detected to reduced the amount of copy/paste coding going around. I suspect the more I add to the player script the more it will have to apply to both players causing me to copy/paste shenanigans.
    - Player One has priority over Player Two in both physics and input operations. Separating/Generalizing the code player code should help with this. But I do not think it'll will be an issue since the game idea does not require any special physics occurrences.
    [Positive Note]
    - I got 2 friends to play-test the game. Feedback was "Well, it functions." and that was my goal for today so its a win in my book.

--- Rockoban/Script/Player.gd
+++ Rockoban/Script/Player.gd
@@ -16,7 +16,9 @@ extends Node2D
 #        				"https://github.com/godotengine/godot/pull/26071"S
 #
 ################################################################################
-
+var p1velocity = Vector2() #Which direction the Player One is moving
+var p2velocity = Vector2() #Which direction Player Two is moving
+export(int) var Speed = 200 #How fast the player moves in pixels/sec
 ################################################################################
 #@class	Player
 #@brief
@@ -28,6 +30,49 @@ func _ready():
 	var err = Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
 	if(err):
 		print(name + " encountered error code: " + String(err))
+
+func _physics_process(delta):
+	P1GetInput()
+	P2GetInput()
+	$P1KinematicBody2D.move_and_collide(p1velocity * delta)
+	$P2KinematicBody2D.move_and_collide(p2velocity*delta)
+
+################################################################################
+#@brief
+#		Determines which direction Player One will move based on contoller
+#		input
+################################################################################
+func P1GetInput():
+	#
+	var direction = Vector2()
+	if Input.is_action_pressed("PlayerOneMoveLeft"):
+		direction += Vector2.LEFT
+	if Input.is_action_pressed("PlayerOneMoveRight"):
+		direction += Vector2.RIGHT
+	if Input.is_action_pressed("PlayerOneMoveUp"):
+		direction += Vector2.UP
+	if Input.is_action_pressed("PlayerOneMoveDown"):
+		direction += Vector2.DOWN
+
+	p1velocity = direction.normalized() * Speed
+
+################################################################################
+#@brief
+#		Determines which direction Player Two will move based on contoller
+#		input
+################################################################################
+func P2GetInput():
+	var direction = Vector2()
+	if Input.is_action_pressed("PlayerTwoMoveLeft"):
+		direction += Vector2.LEFT
+	if Input.is_action_pressed("PlayerTwoMoveRight"):
+		direction += Vector2.RIGHT
+	if Input.is_action_pressed("PlayerTwoMoveUp"):
+		direction += Vector2.UP
+	if Input.is_action_pressed("PlayerTwoMoveDown"):
+		direction += Vector2.DOWN
+
+	p2velocity = direction.normalized() * Speed
 
 ################################################################################
 #@brief
