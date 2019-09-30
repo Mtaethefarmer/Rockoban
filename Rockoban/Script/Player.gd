77f623a08de8e634c53f8a54049c7ea48f861a8a
Devone Reynolds <mtae000@gmail.com>
2019-09-29 22:44:10 -0700

    Day 6
    Day 006[Great]
    
    Goal: Grid-based movement [Complete]
    
    Both players now move independantly of each other, even thou I couldn't
    showcase it. Started animating the players movement and bumping into 
    stuff also. Went into 15 minute overtime to solve my bug, unforunately.
    
    [Improvements]
    - Gotta be careful about the nodes I am editing, I caused my to think there
        was a bug in my code. I animated the Node2D position instead of the 
        Sprite.
    - Same from Day 3
        Another day working only with one controller. I was too late to check one
         out from the library, but I should just bring one from home and leave it
         with my other one.
    
    - Same from Day 5.
        Getting working code is good but I am left at the end to comment my code. 
    
    - I should ask my friends who watch me, for more advice or include them in
        project somehow. I did not notice that one of them had joined so I never
        took the time to say hi. D:
    
    
    [Positive Note]
    - Breaking down the tasks into smaller bits really helps me stay focused on one
        thing at a time. Before the stream I wrote down 5 things that I wanted to 
        get done for this stream and was only able to complete the first 3 tasks.
    
    - Same from Day 5.
        One of my friends and family came by to say hi, made me feel supported
        and not alone. Thanks, fam!
    
    - I felt good to be ready and streaming 5 minutes prior to my initial 
        start-time, I even had a second controller ready.
    
    [Next Goal]Animate the Player(s)...

--- Rockoban/Script/Player.gd
+++ Rockoban/Script/Player.gd
@@ -16,9 +16,9 @@ extends "Pawn.gd"
 #        				"https://github.com/godotengine/godot/pull/26071"S
 #
 ################################################################################
-var p1velocity = Vector2() #Which direction the Player One is moving
-var p2velocity = Vector2() #Which direction Player Two is moving
-export(int) var Speed = 200 #How fast the player moves in pixels/sec
+enum PlayerID{ONE, TWO};
+export(PlayerID) var Id = 1;
+var direction
 ################################################################################
 #@class	Player
 #@brief
@@ -31,53 +31,54 @@ func _ready():
 	if(err):
 		print(name + " encountered error code: " + String(err))
 
-func _physics_process(delta):
-	var rename_var = get_parent().RequestMove(self, P1GetInput())
+func _process(delta):
+	var d = GetInput()
 
-	if rename_var:
-		position = rename_var
+	if not d:
+		return
 
-	#P2GetInput()
-	#$P1KinematicBody2D.move_and_collide(p1velocity * delta)
-	#$P2KinematicBody2D.move_and_collide(p2velocity*delta)
+	var newPosition = get_parent().RequestMove(self, d)
+
+	if newPosition && newPosition != position:
+		set_process(false)
+		$AnimationPlayer.play("walk")
+		$Tween.interpolate_property(self, "icon.position", -direction * 32, Vector2(), $AnimationPlayer.current_animation_length,Tween.TRANS_LINEAR,Tween.EASE_IN)
+		position = newPosition
+		print(newPosition)
+		$Tween.start()
+		yield($AnimationPlayer, "animation_finished")
+		set_process(true)
+	else:
+		$AnimationPlayer.play("bump")
+
 
 ################################################################################
 #@brief
 #		Determines which direction Player One will move based on contoller
 #		input
 ################################################################################
-func P1GetInput():
+func GetInput():
 	#
-	var direction = Vector2()
-	if Input.is_action_pressed("PlayerOneMoveLeft"):
-		direction += Vector2.LEFT
-	if Input.is_action_pressed("PlayerOneMoveRight"):
-		direction += Vector2.RIGHT
-	if Input.is_action_pressed("PlayerOneMoveUp"):
-		direction += Vector2.UP
-	if Input.is_action_pressed("PlayerOneMoveDown"):
-		direction += Vector2.DOWN
-
+	direction = Vector2()
+	if Id == PlayerID.ONE:
+		if Input.is_action_just_pressed("PlayerOneMoveLeft"):
+			direction += Vector2.LEFT
+		elif Input.is_action_just_pressed("PlayerOneMoveRight"):
+			direction += Vector2.RIGHT
+		elif Input.is_action_just_pressed("PlayerOneMoveUp"):
+			direction += Vector2.UP
+		elif Input.is_action_just_pressed("PlayerOneMoveDown"):
+			direction += Vector2.DOWN
+	if Id == PlayerID.TWO:
+		if Input.is_action_pressed("PlayerTwoMoveLeft"):
+			direction += Vector2.LEFT
+		if Input.is_action_pressed("PlayerTwoMoveRight"):
+			direction += Vector2.RIGHT
+		if Input.is_action_pressed("PlayerTwoMoveUp"):
+			direction += Vector2.UP
+		if Input.is_action_pressed("PlayerTwoMoveDown"):
+			direction += Vector2.DOWN
 	return direction
-	#p1velocity = direction.normalized() * Speed
-
-################################################################################
-#@brief
-#		Determines which direction Player Two will move based on contoller
-#		input
-################################################################################
-func P2GetInput():
-	var direction = Vector2()
-	if Input.is_action_pressed("PlayerTwoMoveLeft"):
-		direction += Vector2.LEFT
-	if Input.is_action_pressed("PlayerTwoMoveRight"):
-		direction += Vector2.RIGHT
-	if Input.is_action_pressed("PlayerTwoMoveUp"):
-		direction += Vector2.UP
-	if Input.is_action_pressed("PlayerTwoMoveDown"):
-		direction += Vector2.DOWN
-
-	p2velocity = direction.normalized() * Speed
 
 ################################################################################
 #@brief
